8005438bc5e23d607c492e975a36384d54192f0d
raghav-b <raghavbhardwaj1998@gmail.com>
2020-10-09 21:27:45 +0800

    Fix bottom bar and continue home page

--- my-app/src/components/ClientCard.js
+++ my-app/src/components/ClientCard.js
@@ -9,11 +9,29 @@ function ClientCard(props) {
             <div className="client-image-mask">
                 <img src={props.clientImage}></img>
             </div>
+
             <div className="client-details">
-                
+                <span className="client-name">{props.clientName}</span>
+                <div className="process">
+                    <div>Employment</div>
+                    <div className="status-box">{props.employmentStatus}</div>
+                </div>
+
+                <div className="process">
+                    <span>Housing</span>
+                    <div className="status-box">{props.housingStatus}</div>
+                </div>
+
+                <div className="process">
+                    <span>Counselling</span>
+                    <div className="status-box">{props.counsellingStatus}</div>
+                </div>
+
+                <div className="process">
+                    <span>Operations</span>
+                    <div className="status-box">{props.operationsStatus}</div>
+                </div>
             </div>
-            
-            <div><h2>hello</h2></div>
             
         </div>
     );
--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -6,14 +6,43 @@ import ClientCard from "./ClientCard";
 import "./Home.css";
 
 function Home() {
-    return (
+
+  return (
         <div>
           <h1>Home</h1>
+          
           <div className="card-view">
-            <ClientCard clientImage={require("./img/clients-icon.png")}></ClientCard>
-            <ClientCard clientImage={require("./img/clients-icon.png")}></ClientCard>
-            <ClientCard clientImage={require("./img/clients-icon.png")}></ClientCard>
-            <ClientCard clientImage={require("./img/clients-icon.png")}></ClientCard>
+            <ClientCard 
+              clientImage={require("./img/bryce1.png")}
+              clientName={"Bryce Tan"}
+              employmentStatus={"Initial Engagement"}
+              housingStatus={"Client Eligible"}
+              counsellingStatus={"Completed 1st"}
+              operationsStatus={"Bed Assigned"}/>
+
+            <ClientCard 
+              clientImage={require("./img/raghav1.png")}
+              clientName={"Raghav Bhardwaj"}
+              employmentStatus={"Initial Engagement"}
+              housingStatus={"Client Eligible"}
+              counsellingStatus={"Completed 1st"}
+              operationsStatus={"Bed Assigned"}/>
+
+            <ClientCard 
+              clientImage={require("./img/jia_min1.png")}
+              clientName={"Tan Jia Min"}
+              employmentStatus={"Initial Engagement"}
+              housingStatus={"Client Eligible"}
+              counsellingStatus={"Completed 1st"}
+              operationsStatus={"Bed Assigned"}/>
+            
+            <ClientCard 
+              clientImage={require("./img/gwyneth1.png")}
+              clientName={"Gwyneth Ang"}
+              employmentStatus={"Initial Engagement"}
+              housingStatus={"Client Eligible"}
+              counsellingStatus={"Completed 1st"}
+              operationsStatus={"Bed Assigned"}/>
           </div>
           <NavBar/>
         </div>

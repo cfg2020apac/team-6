06d3ba7a4a1e3fa6475691811e97743db4ed234f
raghav-b <raghavbhardwaj1998@gmail.com>
2020-10-09 22:25:15 +0800

    Finish home page

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -9,7 +9,36 @@ function Home() {
 
   return (
         <div>
-          <h1>Home</h1>
+          <div className="top-bar">
+            <div className="home-bar-level1">
+              <div className="page-title">Home</div>
+              
+              <div className="home-action-button-container">
+                
+                <div className="home-action-button">
+                  <img src={require("./img/add-button.png")}></img>
+                  <div>Add Client</div>
+                </div>
+
+                <div className="home-action-button">
+                  <img src={require("./img/to-do-button.png")}></img>
+                  <div>To-do</div>
+                </div>
+              
+              </div>
+            </div>
+
+            <div className="home-bar-level2">
+
+              <div className="my-clients-text">
+                My Clients
+              </div>
+              <div className="client-search-box">
+                <img src={require("./img/search-icon.png")}></img>
+                <div>Search for clients...</div>
+              </div>  
+            </div>
+          </div>
           
           <div className="card-view">
             <ClientCard 
